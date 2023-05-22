228eae154b18d6da85b1031ac2a209975c535525
Suman Das <sum4ndas@gmail.com>
2023-05-22 15:37:02 +0530

    Add comments to Gameboard

--- battleShip.js
+++ battleShip.js
@@ -22,7 +22,7 @@ const Ship = (length) => {
 };
 
 const Gameboard = () => {
-  // location and ship array needed for receiveAttack function
+  // shipLists will be used for tracking ships and to be able to use actions on them.
   let shipLists = [];
   let missedShots = [];
 
@@ -30,6 +30,7 @@ const Gameboard = () => {
     let shipLength = coOrd.length;
 
     let ship = Ship(shipLength);
+    // location is converted into string to use .includes method.
     ship.location = JSON.stringify(coOrd);
 
     shipLists.push(ship);
@@ -38,8 +39,11 @@ const Gameboard = () => {
   };
 
   const receiveAttack = (coOrd) => {
-    let targetShip = shipLists.filter((x) =>
-      x.location.includes(JSON.stringify(coOrd))
+    // filter through ship's location coordinate to find if attack coordinate hits ship.
+    // if attack hits ship, targetShip will get the 'ship object' to send hit function
+    // to the 'ship object' or the attack coordinate goes into missedShot.
+    let targetShip = shipLists.filter((ship) =>
+      ship.location.includes(JSON.stringify(coOrd))
     );
 
     if (targetShip.length > 0) {
