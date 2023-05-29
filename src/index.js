b0dd7692f6fbe9485f1a5141a9dac5694a3ad550
Suman Das <sum4ndas@gmail.com>
2023-05-29 12:05:05 +0530

    Refactor populatePlayerBoard and populateAiBoard to include ship locaitons

--- src/index.js
+++ src/index.js
@@ -7,51 +7,37 @@ body.style.cssText = "display: flex; justify-content: space-around";
 body.appendChild(uiGameBoard("player"));
 body.appendChild(uiGameBoard("ai"));
 
-// create player and ai ships
-let player = Player();
-let p_ship1 = player.gameboard.placeShip([
-  [2, 4],
-  [2, 5],
-]);
-let p_ship2 = player.gameboard.placeShip([
-  [3, 4],
-  [3, 5],
-  [3, 6],
-]);
-let p_ship3 = player.gameboard.placeShip([
-  [0, 1],
-  [0, 2],
-  [0, 3],
-]);
-let p_ship4 = player.gameboard.placeShip([
-  [7, 5],
-  [7, 6],
-  [7, 7],
-  [7, 8],
-]);
-let p_ship5 = player.gameboard.placeShip([
-  [5, 1],
-  [5, 2],
-  [5, 3],
-  [5, 4],
-  [5, 5],
-]);
-
-let ai = AI();
-let location1 = ai.createValidShipLocation(5);
-let location2 = ai.createValidShipLocation(4);
-let location3 = ai.createValidShipLocation(3);
-let location4 = ai.createValidShipLocation(3);
-let location5 = ai.createValidShipLocation(2);
-
-let ai_ship1 = ai.gameboard.placeShip(location1);
-let ai_ship2 = ai.gameboard.placeShip(location2);
-let ai_ship3 = ai.gameboard.placeShip(location3);
-let ai_ship4 = ai.gameboard.placeShip(location4);
-let ai_ship5 = ai.gameboard.placeShip(location5);
-
 // player board
 function populatePlayerBoard() {
+  // Create player ships
+  let player = Player();
+  let p_ship1 = player.gameboard.placeShip([
+    [2, 4],
+    [2, 5],
+  ]);
+  let p_ship2 = player.gameboard.placeShip([
+    [3, 4],
+    [3, 5],
+    [3, 6],
+  ]);
+  let p_ship3 = player.gameboard.placeShip([
+    [0, 1],
+    [0, 2],
+    [0, 3],
+  ]);
+  let p_ship4 = player.gameboard.placeShip([
+    [7, 5],
+    [7, 6],
+    [7, 7],
+    [7, 8],
+  ]);
+  let p_ship5 = player.gameboard.placeShip([
+    [5, 1],
+    [5, 2],
+    [5, 3],
+    [5, 4],
+    [5, 5],
+  ]);
   // console.log(player.gameboard.shipLocationLists);
   let playerBoard = document.getElementById("player");
   // console.log(playerBoard);
@@ -69,6 +55,20 @@ populatePlayerBoard();
 
 // ai board printing for test
 function populateAiBoard() {
+  // Place ai ships.
+  let ai = AI();
+  let location1 = ai.createValidShipLocation(5);
+  let location2 = ai.createValidShipLocation(4);
+  let location3 = ai.createValidShipLocation(3);
+  let location4 = ai.createValidShipLocation(3);
+  let location5 = ai.createValidShipLocation(2);
+
+  let ai_ship1 = ai.gameboard.placeShip(location1);
+  let ai_ship2 = ai.gameboard.placeShip(location2);
+  let ai_ship3 = ai.gameboard.placeShip(location3);
+  let ai_ship4 = ai.gameboard.placeShip(location4);
+  let ai_ship5 = ai.gameboard.placeShip(location5);
+
   // console.log(ai.allAIShipLocations);
   ai.allAIShipLocations.forEach((location) => {
     let x = JSON.stringify(location);
